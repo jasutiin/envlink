f469c27c0fc8494965bca2c3967fd9c5f475a98e
justine <justinemangaliman@gmail.com>
2026-02-15 15:11:43 -0700

    copy paste login functionality to register

--- internal/cli/register.go
+++ internal/cli/register.go
@@ -1,7 +1,11 @@
 package cli
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -15,6 +19,52 @@ var registerCmd = &cobra.Command{
   Short: "Register to envlink.",
   Long:  `Register to envlink.`,
   Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("Register command ran.")
+    register()
   },
+}
+
+func register() {
+  var email string;
+  var password string;
+
+  fmt.Printf("Email: ")
+  fmt.Scanln(&email)
+
+  fmt.Printf("Password: ")
+  fmt.Scanln(&password)
+
+  if email != "" {
+    fmt.Println("email provided")
+  } else {
+    fmt.Println("email not provided")
+  }
+
+  if password != "" {
+    fmt.Println("password provided")
+  } else {
+    fmt.Println("password not provided")
+  }
+
+  jsonStr := []byte(fmt.Sprintf(`{"email":"%s","password":"%s"}`, email, password))
+  payload := bytes.NewBuffer(jsonStr)
+  client := &http.Client{Timeout: 10 * time.Second}
+  req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/auth/register", payload)
+
+  if err != nil {
+    fmt.Println("error on creating new POST req for register")
+  }
+
+  req.Header.Set("Content-Type", "application/json")
+
+  resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Error performing request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		fmt.Printf("request successful with status code: %d\n", resp.StatusCode)
+	} else {
+		fmt.Printf("request failed with status code: %d\n", resp.StatusCode)
+	}
 }
