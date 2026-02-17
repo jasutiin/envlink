b2939c69bab9a0dd671a31234c48c75682e361fe
Justine <justinemangaliman@gmail.com>
2026-02-16 19:12:42 -0700

    callback url either points to railway or localhost
    so that it can work on both environments

--- internal/server/auth/auth.go
+++ internal/server/auth/auth.go
@@ -27,8 +27,17 @@ func NewAuth() {
 	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
 	store := sessions.NewCookieStore([]byte(key))
 
+	var url string
+	domain := os.Getenv("RAILWAY_PUBLIC_DOMAIN")
+	port := os.Getenv("PORT")
+	if domain == "" {
+		url = fmt.Sprintf("http://localhost:%s/api/v1/auth/google/callback", port)
+	} else {
+		url = fmt.Sprintf("https://%s/api/v1/auth/google/callback", domain)
+	}
+
 	gothic.Store = store
 	goth.UseProviders(
-		google.New(googleClientId, googleClientSecret, "http://localhost:8080/api/v1/auth/google/callback"),
+		google.New(googleClientId, googleClientSecret, url),
 	)
 }
