5fd73479e8e3b537959943f2e2b066ff87e7d907
Justine <justinemangaliman@gmail.com>
2026-02-22 17:17:52 -0700

    add more error handling in server entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -10,21 +11,42 @@ import (
 	"github.com/jasutiin/envlink/internal/server/projects"
 	"github.com/jasutiin/envlink/internal/server/pull"
 	"github.com/jasutiin/envlink/internal/server/push"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatalf("Could not load environment variables!")
+	}
+	
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("Port was not provided!")
+	}
+
 	server := gin.Default()
 	api := server.Group("/api/v1")
 	db := database.CreateDB()
 	database.AutoMigrate(db) // creates tables if they don't exist
 
-	auth.NewAuth()
+	key := os.Getenv("COOKIE_SESSION_KEY")
+	if key == "" {
+		key = "dev-key-123"
+	}
+	
+	domain := os.Getenv("RAILWAY_PUBLIC_DOMAIN")
+
+	err = auth.NewAuth(port, domain, key)
+	if err != nil {
+		log.Fatalf("Failed to initialize auth: %s", err)
+	}
+
 	auth.AuthRouter(api, db)
 	push.PushRouter(api)
 	pull.PullRouter(api)
 	projects.ProjectsRouter(api)
 
-	port := os.Getenv("PORT")
 	fmt.Printf("listening on port %s", port)
 	server.Run("0.0.0.0:" + port)
 };
--- internal/server/auth/auth.go
+++ internal/server/auth/auth.go
@@ -1,35 +1,35 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/gorilla/sessions"
-	"github.com/joho/godotenv"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
 )
 
-const (
-	key = "randomString"
-	MaxAge = 86400 * 30
-	IsProd = false
-)
-
-func NewAuth() {
-	err := godotenv.Load()
-	if err != nil{
-		fmt.Println("problem loading .env file")
+/*
+This function is called to initialize the gothic package with the external
+providers we will be using for OAuth.
+*/
+func NewAuth(port string, domain string, key string) error {
+	googleClientId := os.Getenv("GOOGLE_CLIENT_ID")
+	if googleClientId == "" {
+		return errors.New("Google Client Id was not provided!")
 	}
 
-	googleClientId := os.Getenv("GOOGLE_CLIENT_ID")
 	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	if googleClientSecret == "" {
+		return errors.New("Google Client Secret was not provided!")
+	}
+
 	store := sessions.NewCookieStore([]byte(key))
 
 	var url string
-	domain := os.Getenv("RAILWAY_PUBLIC_DOMAIN")
-	port := os.Getenv("PORT")
+
 	if domain == "" {
 		url = fmt.Sprintf("http://localhost:%s/api/v1/auth/google/callback", port)
 	} else {
@@ -40,4 +40,6 @@ func NewAuth() {
 	goth.UseProviders(
 		google.New(googleClientId, googleClientSecret, url),
 	)
+
+	return nil
 }
